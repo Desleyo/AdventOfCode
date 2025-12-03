150c544b9f5f4cbb1a98f527a7ee8bb28c0a3ace
Desleyo <desleyoord@gmail.com>
2025-12-03 23:44:04 +0100

    Completed day1_1, started on day1_2

---
+++ AdventOfCode2025/Day1_1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Day1_1;
+
+class Program
+{
+    private static string[] _input = File.ReadAllLines("input.txt");
+    private static int _dial = 50;
+
+    static void Main(string[] args)
+    {
+        int count = 0;
+
+        foreach (var line in _input)
+        {
+            string dir = line[..1];
+            int steps = int.Parse(line[1..]);
+
+            _dial = dir == "R" ? _dial + steps : _dial - steps;
+
+            while (_dial < 0 || _dial > 99)
+            {
+                if (_dial < 0)
+                {
+                    _dial = 100 + _dial;
+                }
+                else if (_dial > 99)
+                {
+                    _dial = -1 + (_dial - 99);
+                }
+            }
+
+            if (_dial == 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
---
+++ AdventOfCode2025/Day1_2/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Day1_2;
+
+class Program
+{
+    private static string[] _input = File.ReadAllLines("input.txt");
+    //private static int _dial = 50;
+
+    static void Main(string[] args)
+    {
+        int _dial = 50;
+        int count = 0;
+
+        foreach (var line in _input)
+        {
+            string dir = line[..1];
+            int steps = int.Parse(line[1..]);
+            
+            Console.WriteLine($"Dial: {_dial}");
+            Console.WriteLine(line);
+
+            bool excluded = _dial == 0 && steps < 100;
+            
+            _dial = dir == "R" ? _dial + steps : _dial - steps;
+            
+            while (_dial < 0 || _dial > 99)
+            {
+                if (_dial < 0)
+                {
+                    _dial = 100 + _dial;
+
+                    if (!excluded)
+                    {
+                        count++;
+                    }
+                }
+                else if (_dial > 99)
+                {
+                    _dial = -1 + (_dial - 99);
+                    count++;
+                }
+            }
+
+            if (_dial == 0 && steps < 100 && dir == "L")
+            {
+                count++;
+            }
+            
+            Console.WriteLine($"Count: {count}");
+        }
+
+        Console.WriteLine(count);
+    }
+}
